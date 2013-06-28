4ec8e4b87d0c155c0b9ffcdfe331293976a5f201
otiai10 <otiai10@gmail.com>
2013-06-28 09:24:23 +0900

    thread training

---
+++ _lab/continuous_th.py
@@ -0,0 +1,33 @@
+import threading
+import time
+
+def work():
+  """thread worker function"""
+  for i in range(5):
+    print "This is %s => seq %s" % (threading.currentThread().getName(), i)
+    time.sleep(1)
+  return
+
+if __name__ == "__main__":
+
+  threads = []
+  for i in range(10):
+    t = threading.Thread(target=work)
+    threads.append(t)
+    t.start()
+
+  # thread health check and clean up
+  while 1:
+    all_dead = True
+    for t in threads:
+      if t.is_alive():
+        all_dead = False
+      else:
+        print "%s is dead" % t.getName()
+        threads.remove(t)
+    if all_dead:
+      print "<< All Dead >>"
+      break
+ 
+  print "***** END OF MAIN *****" 
+  print threads # expected to be [], but "sometimes" it contains dead thread!!
